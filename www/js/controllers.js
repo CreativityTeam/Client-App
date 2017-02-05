25e0befb42ddeb41568e722b0e98616c91ed972e
Nhat Nguyen <nhatdev@outlook.com>
2017-02-05 23:51:13 +0700

    Integrate Google Map into My Oder

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -36,7 +36,6 @@ angular.module('app.controllers', [])
         $http.get(API_ENDPOINT.url + '/api/orders/findinfobyuser/' + AuthService.userInforIdSave()).success(function(response){
             if(response.success){
                 $scope.orderCurrentUser = response.data;
-                console.log(response.data)
             }
         });   
     }
@@ -87,6 +86,32 @@ angular.module('app.controllers', [])
    
 .controller('sHIPPINGCtrl', function ($scope, $stateParams) {
 
+    function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15
+        });
+        var infoWindow = new google.maps.InfoWindow({map: map});
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+    initMap();
 
 })
       
