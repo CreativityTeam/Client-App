234837fc6fe0f2af108ce5ecc84fb97148c551df
Nhat Nguyen <nhatdev@outlook.com>
2017-02-05 18:18:11 +0700

    Intergate Google Map

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -46,7 +46,33 @@ angular.module('app.controllers', [])
 })
    
 .controller('aroundCtrl', function ($scope, $stateParams) {
+    
+   function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15
+        });
+        var infoWindow = new google.maps.InfoWindow({map: map});
 
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+    initMap();
 
 })
    
